2fd89b8824eb3cda92f972af64154a1dc5ae467c
JITHU FRANCIS <117467296+jithu-francis017@users.noreply.github.com>
2023-06-13 18:44:28 +0530

    Update settings.py

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sc^jwewyjz=6+ztdd6v1m9ks+l-#4la7d_7f1%n)&60t-l2bai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
